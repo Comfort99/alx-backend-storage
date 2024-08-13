f0d5f176b6496241dd8475b0bad73fbe6293037b
Comfort99 <shingangecomfort@gmail.com>
2024-08-14 01:27:18 +0200

    filter

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -9,4 +9,12 @@ def schools_by_topic(mongo_collection, topic):
     Function that returns a filtered lists of collections
     containing a specific topic
     """
-    return list(mongo_collection.find_many({'topic': topic}))
+    topic_filter = {
+        'topic': {
+            '$elemMatch': {
+                '$eq': topic,
+            }
+        }
+    }
+
+    return list(mongo_collection.find(topic_filter))
